257a66f1f25dc9f5cebf54691925558d2fda229d
gagan-0108 <yadavgagan61@gmail.com>
2026-05-11 17:31:07 +0530

    add dark mode in the status bar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,6 +9,8 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { StatusBar } from "expo-status-bar";
+
 export default function RootLayout() {
     const [loaded] = useFonts({
         "Poppins-Regular": Poppins_400Regular,
@@ -24,6 +26,7 @@ export default function RootLayout() {
 
     return (
         <SafeAreaProvider>
+            <StatusBar style="dark" />
             <Stack screenOptions={{ headerShown: false }} />
         </SafeAreaProvider>
     );
